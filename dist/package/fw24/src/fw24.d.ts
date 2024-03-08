966a2a857d25d373b46063fcb349b74692e4961c
Nitin Yadav <nitinrajyadav@outlook.com>
2024-03-08 19:53:35 +0530

    Revert: bad changed after merge

--- dist/package/fw24/src/fw24.d.ts
+++
@@ -1,8 +0,0 @@
-export * from "./application.class";
-export * from "./stacks/api-gateway.stack";
-export * from "./stacks/amplify.stack";
-export * as Auditor from './audit';
-export * as Validator from './validation';
-export * as Logger from './logging';
-export * as EventDispatcher from './event';
-export * from './entity';
