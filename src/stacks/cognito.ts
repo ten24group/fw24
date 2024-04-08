103097bbc9be1c048360616e018c0d8e95fd21fe
Miguel Targa <miguel@games.com>
2024-04-08 18:01:01 -0400

    Port PR #29

--- src/stacks/cognito.ts
+++ src/stacks/cognito.ts
@@ -1,16 +1,17 @@
 import { 
+    CfnIdentityPool, 
+    CfnIdentityPoolRoleAttachment,
+    CfnUserPoolGroup,
     UserPool, 
     UserPoolClient, 
-    VerificationEmailStyle, 
-    CfnIdentityPool, 
     UserPoolProps, 
-    UserPoolOperation 
+    UserPoolOperation,
+    VerificationEmailStyle, 
 } from "aws-cdk-lib/aws-cognito";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { CognitoUserPoolsAuthorizer } from "aws-cdk-lib/aws-apigateway";
-import { PolicyStatement } from "aws-cdk-lib/aws-iam";
-import { Duration } from "aws-cdk-lib";
-import { readFileSync } from "fs";
+import { Role } from "aws-cdk-lib/aws-iam";
+import { Duration, RemovalPolicy } from "aws-cdk-lib";
 import { CognitoAuthRole } from "../constructs/cognito-auth-role";
 import { LambdaFunction } from "../constructs/lambda-function";
 import { Fw24 } from "../core/fw24";
@@ -21,11 +22,16 @@ export interface ICognitoConfig {
     userPool: {
         props: UserPoolProps;
     };
-    policyFilePath?: string;
+    policyFilePaths?: string[];
     triggers?: {
         trigger: UserPoolOperation;
         lambdaFunctionPath: string;
     }[];
+    groups?: {
+        name: string;
+        precedence?: number;
+        policyFilePaths: string[];
+    }[]
 }
 
 export class CognitoStack implements IStack {
@@ -41,18 +47,18 @@ export class CognitoStack implements IStack {
     public construct() {
         console.log("Cognito construct");
 
-        const fw24 = Fw24.getInstance();
         const userPoolConfig = this.stackConfig.userPool;
-        const mainStack = fw24.getStack("main");
-        var namePrefix = `${fw24.appName}`;
+        const mainStack = this.fw24.getStack("main");
+        var namePrefix = `${this.fw24.appName}`;
 
-        if(fw24.get("tenantId")){
-            namePrefix = `${namePrefix}-${fw24.get("tenantId")}`
+        if(this.fw24.get("tenantId")){
+            namePrefix = `${namePrefix}-${this.fw24.get("tenantId")}`
         }
 
-        // TODO: Add ability to create multiple user pools
+        const userPoolName = userPoolConfig.props.userPoolName || 'default';
+
         // TODO: Add ability to create multi-tenant user pools
-        const userPool = new UserPool(mainStack, `${namePrefix}-userPool`, {
+        const userPool = new UserPool(mainStack, `${namePrefix}-${userPoolName}userPool`, {
             selfSignUpEnabled: userPoolConfig.props.selfSignUpEnabled,
             accountRecovery: userPoolConfig.props.accountRecovery,
             userVerification: {
@@ -71,7 +77,8 @@ export class CognitoStack implements IStack {
                 requireDigits: true,
                 requireSymbols: true,
                 tempPasswordValidity: Duration.days(3),
-            }
+            },
+            removalPolicy: userPoolConfig.props.removalPolicy || RemovalPolicy.RETAIN,
         });
 
         const userPoolClient = new UserPoolClient(mainStack, `${namePrefix}-userPoolclient`, {
@@ -97,36 +104,64 @@ export class CognitoStack implements IStack {
             }],
         });
 
-        // IAM role for authenticated users
-        const authenticatedRole = new CognitoAuthRole(mainStack, "CognitoAuthRole", {
-            identityPool,
-        });
-
-        // apply the policy to the role
-        if (this.stackConfig.policyFilePath) {
-            // read file from policyFilePath
-            const policyfile: string = readFileSync(this.stackConfig.policyFilePath, 'utf8');
-            authenticatedRole.role.addToPolicy(
-                new PolicyStatement({
-                    effect: JSON.parse(policyfile).Effect,
-                    actions: JSON.parse(policyfile).Action,
-                    resources: JSON.parse(policyfile).Resource,
-                })
-            );
+        // configure identity pool role attachment
+        const identityProvider = userPool.userPoolProviderName + ':' + userPoolClient.userPoolClientId;
+        const roleAttachment: any = {
+            identityPoolId: identityPool.ref,
         }
+
+         // create user pool groups
+        if (this.stackConfig.groups) {
+            for (const group of this.stackConfig.groups) {
+                // create a role for the group
+                const policyFilePaths = group.policyFilePaths;
+                const role = new CognitoAuthRole(mainStack, `${namePrefix}-${group.name}-CognitoAuthRole`, {
+                    identityPool,
+                    policyFilePaths,
+                }) as Role;
+
+                new CfnUserPoolGroup(mainStack, `${namePrefix}-${group.name}-group`, {
+                    groupName: group.name,
+                    userPoolId: userPool.userPoolId,
+                    roleArn: role.roleArn,
+                    precedence: group.precedence || 0,
+                });
+            }
+            // configure role mapping
+            roleAttachment.roleMappings = {
+                "userpool": {
+                    type: "Token",
+                    ambiguousRoleResolution: "Deny",
+                    identityProvider: identityProvider,
+                }
+            }
+        }
+
+        // IAM role for authenticated users if no groups are defined
+        const policyFilePaths = this.stackConfig.policyFilePaths;
+        const authenticatedRole = new CognitoAuthRole(mainStack, `${namePrefix}-CognitoAuthRole`, {
+            identityPool,
+            policyFilePaths
+        }) as Role;
+
+        roleAttachment.roles = {};
+        roleAttachment.roles.authenticated = authenticatedRole.roleArn;
+
+        new CfnIdentityPoolRoleAttachment(mainStack, `${namePrefix}-IdentityPoolRoleAttachment`, roleAttachment);
         // create triggers
         if (this.stackConfig.triggers) {
             for (const trigger of this.stackConfig.triggers) {
                 const lambdaTrigger = new LambdaFunction(mainStack, `${namePrefix}-${trigger.trigger}-lambdaFunction`, {
                     entry: trigger.lambdaFunctionPath,
-                    layerArn: fw24.getLayerARN(),
+                    layerArn: this.fw24.getLayerARN(),
                 }) as NodejsFunction;
                 userPool.addTrigger(trigger.trigger, lambdaTrigger);
             }
         }
 
-        fw24.set("userPoolID", userPool.userPoolId);
-        fw24.set("userPoolClientID", userPoolClient.userPoolClientId);
-        fw24.setCognitoAuthorizer(this.stackConfig.userPool.props.userPoolName || 'default', userPoolAuthorizer, true);
+        this.fw24.set("userPoolID", userPool.userPoolId);
+        this.fw24.set("userPoolClientID", userPoolClient.userPoolClientId);
+        this.fw24.set("identityPoolID", identityPool.ref);
+        this.fw24.setCognitoAuthorizer(userPoolName, userPoolAuthorizer, true);
     }
 }
