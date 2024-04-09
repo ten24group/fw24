3b42622ad7e1f9f3d8e0a39213086caaa4b9ef3d
Sumit Verma <sumit.verma@ten24web.com>
2024-04-08 20:12:59 -0400

    use topic arn

--- src/client/util.ts
+++ src/client/util.ts
@@ -1,6 +1,6 @@
 export const Environment = {
     emailQueueUrl: process.env.EMAIL_QUEUE_URL || '',
     queueUrl: (queueName: string) => process.env[queueName + '_queueUrl'] || '',
-    topicUrl: (topicName: string) => process.env[topicName + '_topicUrl'] || '',
+    topicArn: (topicName: string) => process.env[topicName + '_topicArn'] || '',
     bucketName: (bucketName: string) => process.env['bucket_' + bucketName] || ''
 }
