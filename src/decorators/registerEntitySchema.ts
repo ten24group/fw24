ae05e61f2a3cf80a6af0c9c4452125694a183447
Nitin Yadav <nitinrajyadav@outlook.com>
2025-04-28 08:37:26 +0530

    Fix: logs in registerEntitySchema to not log full objects

--- src/decorators/registerEntitySchema.ts
+++ src/decorators/registerEntitySchema.ts
@@ -15,12 +15,12 @@ export type EntitySchemaProviderOptions = OmitAnyKeys<ProviderOptions<any>, 'pro
     doNotAutoRegisterEntityService?: boolean;
 };
 
-function makeEntityServiceName(entityName: string){
+function makeEntityServiceName(entityName: string) {
     return `${pascalCase(camelCase(entityName))}Service`
 }
 
-export function registerEntitySchema<T extends EntitySchema<any, any, any>>(options: EntitySchemaProviderOptions & { forEntity: T['model']['entity']}){
-    
+export function registerEntitySchema<T extends EntitySchema<any, any, any>>(options: EntitySchemaProviderOptions & { forEntity: T[ 'model' ][ 'entity' ] }) {
+
     const entitySchemaToken = `${options.forEntity}Schema`;
 
     const optionsCopy = {
@@ -31,15 +31,15 @@ export function registerEntitySchema<T extends EntitySchema<any, any, any>>(opti
 
     let container: IDIContainer | undefined;
 
-    if(!options.providedIn || options.providedIn === 'ROOT'){
+    if (!options.providedIn || options.providedIn === 'ROOT') {
         container = DIContainer.ROOT;
-    } else if(options.providedIn instanceof DIContainer){
+    } else if (options.providedIn instanceof DIContainer) {
         container = options.providedIn!;
-    } else if(typeof options.providedIn === 'function') {
+    } else if (typeof options.providedIn === 'function') {
 
         // Check if the providedIn is a class constructor
         const moduleMetadata = getModuleMetadata(options.providedIn);
-        
+
         if (!moduleMetadata) {
             throw new Error(
                 `Invalid providedIn option. No module metadata found for ${options.providedIn.name}; ensure the class is decorated with @DIModule({...}).`
@@ -47,11 +47,11 @@ export function registerEntitySchema<T extends EntitySchema<any, any, any>>(opti
         }
 
         moduleMetadata.addProvider(optionsCopy);
-        
+
         container = moduleMetadata.container;
     }
 
-    if(!container){
+    if (!container) {
         throw new Error(
             `Invalid providedIn option; no container could be resolved. Ensure it is either 'ROOT' or an instance of DI-container or a class decorated with @DIModule({...}).`
         );
@@ -60,15 +60,15 @@ export function registerEntitySchema<T extends EntitySchema<any, any, any>>(opti
     try {
         container.register(optionsCopy);
     } catch (error) {
-        DefaultLogger.error(`registerEntitySchema:: Error registering ${options.forEntity} schema with container:`, container);
+        DefaultLogger.error(`registerEntitySchema:: Error registering ${options.forEntity} schema with container:`, { container: container.containerId, error });
         throw error;
     }
 
-    if(options.doNotAutoRegisterEntityService){
+    if (options.doNotAutoRegisterEntityService) {
         return;
     }
 
-    class AutoGenEntityService extends BaseEntityService<any> {        
+    class AutoGenEntityService extends BaseEntityService<any> {
         constructor(
             readonly entityConfiguration: EntityConfiguration,
             readonly schema: any,
@@ -82,19 +82,19 @@ export function registerEntitySchema<T extends EntitySchema<any, any, any>>(opti
 
     try {
         container.register({
-            priority: -1, 
+            priority: -1,
             type: 'service',
             provide: AutoGenEntityService,
             forEntity: optionsCopy.forEntity,
 
-            deps: [DI_TOKENS.DYNAMO_ENTITY_CONFIGURATIONS, entitySchemaToken, DIContainer],
-            useFactory: (config: EntityConfiguration, schema: any, diContainer: IDIContainer ) => {
-                const service = new AutoGenEntityService(config, schema, diContainer );
+            deps: [ DI_TOKENS.DYNAMO_ENTITY_CONFIGURATIONS, entitySchemaToken, DIContainer ],
+            useFactory: (config: EntityConfiguration, schema: any, diContainer: IDIContainer) => {
+                const service = new AutoGenEntityService(config, schema, diContainer);
                 return service;
             },
         })
     } catch (error) {
-        DefaultLogger.error(`registerEntitySchema:: Error registering ${options.forEntity} default-service with container:`, container);
+        DefaultLogger.error(`registerEntitySchema:: Error registering ${options.forEntity} default-service with container:`, { container: container.containerId, error });
         throw error;
     }
 }
